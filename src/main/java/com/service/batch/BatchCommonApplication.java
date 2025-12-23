0085fbf212677136837274722b0a32d827f42a76
jeonDev <jjhwhdgus0727@gmail.com>
2025-12-23 15:41:06 +0900

    Initial commit

---
+++ src/main/java/com/service/batch/BatchCommonApplication.java
@@ -0,0 +1,13 @@
+package com.service.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchCommonApplication.class, args);
+    }
+
+}

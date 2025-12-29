57a5fc688b8dd8fcbedf34d8f4f8473e08611802
jeonDev <jjhwhdgus0727@gmail.com>
2025-12-29 22:21:03 +0900

    feat : 배치 메타 데이터베이스 설정 조건 추가

--- src/main/java/com/service/batch/common/config/BatchDataSourceConfig.java
+++ src/main/java/com/service/batch/common/config/BatchDataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.service.batch.common.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
+@ConditionalOnProperty(prefix = "batch.datasource", name = "driver-class-name")
 public class BatchDataSourceConfig {
 
     @Bean
